0aa035903601b6dc2278e00876215f9ef370bd3c
tsvetkovpro <285019@gmail.com>
2016-09-08 12:01:30 +0500

    + landing

--- _software-development/bitbucket/webpack/04-dynamic-2/server.js
+++
@@ -1,9 +0,0 @@
-var static = require('node-static');
-var file = new static.Server('./public');
-
-require('http').createServer(function (request, response) {
-  if (!/\./.test(request.url)) {
-    request.url = '/';
-  }
-  file.serve(request, response);
-}).listen(3000);
--- software-development/examples/vuejs2-authentication/README.md
+++
@@ -1,2 +0,0 @@
-# vue2-auth
-Authentication using VueJS 2.0, Express, and Passport
